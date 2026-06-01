4cb117ab9be70174607b377a4b291c370b9dc92a
jianglee <lehoanggiang03@gmail.com>
2026-06-02 00:05:58 +0700

    bai 1 - buoi 1 - update

---
+++ buoi_1/info.sh
@@ -0,0 +1,11 @@
+#!home/jianglee/linux_embedded/exercises/buoi_1
+
+HOME_DIR=$HOME
+CURRENT_USER=$USER
+CURRENT_SHELL=$SHELL
+CURRENT_DIR=$PWD
+
+echo "Hello, $CURRENT_USER"
+echo "Your home directory is: $HOME_DIR"
+echo "Your shell is: $CURRENT_SHELL"
+echo "Current directory: $CURRENT_DIR"
